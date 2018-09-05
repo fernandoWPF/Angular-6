553dbacf6d8771b1086df5fcf90d393abdea3bda
fernando <fernando.fernandes@matera.com>
2018-09-05 18:39:04 -0300

    Criação do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projeto';
+  title = 'Projeto';
+  clientes = [];
+  cliente = "";
+
+  addCliente(){
+    this.clientes.push(this.cliente);
+  }
+
 }
